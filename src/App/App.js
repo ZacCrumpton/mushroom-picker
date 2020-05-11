57e29efc4030cae3bf1ce5bace9cd84d8716eaf3
Zac Crumpton <z.crumpton@gmail.com>
2020-05-11 02:29:29 -0500

    finished setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>MUSHROOM PICKERS</h2>
+        <button className="btn btn-dark"><i className="fas fa-rocket"></i></button>
+      </div>
+    );
+  }
 }
 
 export default App;
